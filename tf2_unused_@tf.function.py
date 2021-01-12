ebbf062c95e4193570b323ed4bcaec2020621db4
monta0315 <monta0315@docomo.ne.jp>
2021-01-12 17:13:49 +0900

    frozengraphを作成したが、tensorflowC++ではエラーが出て読み込むことができなかった

--- tf2_unused_@tf.function.py
+++ tf2_unused_@tf.function.py
@@ -5,6 +5,7 @@ from tensorflow.keras.layers import Dense, Flatten, Conv2D
 from tensorflow.keras import Model
 import tensorflow.keras.layers as layers
 import numpy as np
+from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
 
 #tf2.0なのに@tf.functionを使用していないので死ぬほど時間かかります
 
@@ -39,6 +40,21 @@ optim = tf.keras.optimizers.Adam()
 model.compile(optimizer=optim, loss=loss, metrics=[acc])
 
 model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=5, batch_size=128)
+
+full_model = tf.function(lambda x: model(x))
+full_model = full_model.get_concrete_function(
+    x=tf.TensorSpec(model.inputs[0].shape, model.inputs[0].dtype)
+)
+
+# Get frozen ConcreteFunction
+frozen_func = convert_variables_to_constants_v2(full_model)
+frozen_func.graph.as_graph_def()
+
+tf.io.write_graph(graph_or_graph_def=frozen_func.graph,
+                  logdir="./frozen_models",
+                  name="frozen_graph_tf2.pb",
+                  as_text=True)
+
 
 if not os.path.isdir(f_model):
   os.makedirs(f_model)
